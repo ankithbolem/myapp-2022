47a36a196b6969d34ca7d31925ec61b588a41cf5
ankithbolem <100229084+ankithbolem@users.noreply.github.com>
2022-09-17 13:35:38 +0530

    Update tomcat.groovy

--- var/tomcat.groovy
+++ var/tomcat.groovy
@@ -1,16 +1,3 @@
-@Library("prasadlibs") _
-pipeline{
-    agent any
-    stages{
-        stage("Maven Build"){
-            steps{
-                sh 'mvn clean package -DskipTests=true'
-            }
-        }
-        stage(" Dev Tomcat Deploy"){
-            steps{
-                tomcatDeploy("172.31.1.213","ec2-user","tomcat-dev")
-            }
-        }
-    }
+def call ("ip"){
+    echo ${ip}
 }
